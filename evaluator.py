bff5717a0af18bdaeea7b19cf48abad799a096cf
Derek Smith <github@sandy.simplelogin.com>
2025-04-07 15:42:41 -0600

    OOPify evaluator

--- evaluator.py
+++ evaluator.py
@@ -1,23 +1,29 @@
 from math import prod
 
-def prob_spam_given_token(t):
-    if t not in good_counts and t not in bad_counts:
-        return 0.4
-    
-    rg = min(1, 2 * good_counts[t] / num_good)
-    rb = min(1, bad_counts[t] / num_bad)
-    return max(0.01, min(0.99, rb / (rg + rb)))
+from trainer import count_tokens # this is a weird dependence
 
-def prob_spam(filename):
-    token_interest = {t: abs(0.5 - prob_spam_given_token(t))
-                      for t in count_tokens(filename).keys()}
+class Evaluator:
+    def __init__(self):
+        self.model = Model.read('model/model.pickle')
 
-    most_interesting = sorted(token_interest, key=token_interest.get, reverse=True)[:15]
+    def prob_spam_given_token(self, t):
+        if t not in good_counts and t not in bad_counts:
+            return 0.4
+        
+        rg = min(1, 2 * good_counts[t] / num_good)
+        rb = min(1, bad_counts[t] / num_bad)
+        return max(0.01, min(0.99, rb / (rg + rb)))
 
-    positive = prod([prob_spam_given_token(t)     for t in most_interesting])
-    negative = prod([1 - prob_spam_given_token(t) for t in most_interesting])
-    
-    return positive / (positive + negative)
+    def prob_spam(self, text: str):
+        token_interest = {t: abs(0.5 - self.prob_spam_given_token(t))
+                        for t in count_tokens(text).keys()}
 
-def is_spam(filename):
-    return True if prob_spam(filename) > 0.9 else False
+        most_interesting = sorted(token_interest, key=token_interest.get, reverse=True)[:15]
+
+        positive = prod([prob_spam_given_token(t)     for t in most_interesting])
+        negative = prod([1 - prob_spam_given_token(t) for t in most_interesting])
+        
+        return positive / (positive + negative)
+
+    def is_spam(self, text: str):
+        return True if self.prob_spam(text) > 0.9 else False
